46f19a24a5f8181d0e22afb1ccfe57be0649d627
ghbxq <bxq147@qq.com>
2016-09-21 16:47:28 +0800

    change

--- testapp/src/androidTest/java/com/prac/buxiaoqing/testapp/MainActivityInstrumentationTest.java
+++ testapp/src/androidTest/java/com/prac/buxiaoqing/testapp/MainActivityInstrumentationTest.java
@@ -8,10 +8,14 @@ package com.prac.buxiaoqing.testapp;
  *
  */
 
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.text.TextUtils;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -19,13 +23,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.instanceOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -38,33 +47,65 @@ public class MainActivityInstrumentationTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
-    @Test(timeout = 1000)
+    @Test(timeout = 5000)
     public void sayHello() {
-        onView(withId(R.id.editText)).perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard()); //line 1
+//        onView(withId(R.id.editText)).perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard()); //line 1
+//
+//        onView(withText("Say hello!")).perform(click()); //line 2
+//
+//        String expectedText = PRE_FIXED + STRING_TO_BE_TYPED + "!";
+//        onView(withId(R.id.textView)).check(matches(withText(expectedText))); //line 3
+        //onView  只能查找那些已经先是在屏幕上的View,对于那些ListView 或者GridView 上的还没有显示到屏幕上的数据,就用onData来查找。
 
-        onView(withText("Say hello!")).perform(click()); //line 2
+        //AdapterView   查找数据
+        //onData(allOf(is(instanceOf(MainActivity.SearchItem.class)),teacherSearchItemWithName("item:10"))).inAdapterView(withId(R.id.listview));
 
-        String expectedText = PRE_FIXED + STRING_TO_BE_TYPED + "!";
-        onView(withId(R.id.textView)).check(matches(withText(expectedText))); //line 3
+//        onView(allOf(withText("halo!"), hasSibling(withText("Say hello!"))))
+//                .perform(click());
+
+        onData(teacherSearchItemWithName("item:20"))
+                .onChildView(withId(R.id.tv_size))
+                .perform(click());
+
+
     }
 
+    /**
+     * 查找指定关键字的搜索条件
+     * @param name 需要搜索的关键字
+     */
+    public static Matcher<Object> teacherSearchItemWithName(final String name) {
+        return new BoundedMatcher<Object, MainActivity.SearchItem>(MainActivity.SearchItem.class) {
+            @Override
+            protected boolean matchesSafely(MainActivity.SearchItem item) {
+                return item != null
+                        && !TextUtils.isEmpty(item.getKeyword())
+                        && item.getKeyword().equals(name);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("SearchItem has Name: " + name);
+            }
+        };
+    }
 
     @After
     public void finish() {
 
     }
 
-
-    @AfterClass
-    public void after() {
-
-    }
-
-
-    @BeforeClass
-    public void before() {
-
-    }
+//
+//    @AfterClass
+//    public void after() {
+//
+//    }
+//
+//
+//    @BeforeClass
+//    public void before() {
+//
+//    }
 
 
 }
--- testapp/src/main/java/com/prac/buxiaoqing/testapp/MainActivity.java
+++ testapp/src/main/java/com/prac/buxiaoqing/testapp/MainActivity.java
@@ -2,16 +2,37 @@ package com.prac.buxiaoqing.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listview);
+
+        List<SearchItem> list = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            list.add(new SearchItem("item:" + i));
+        }
+
+        mAdapter adapter = new mAdapter();
+        adapter.setList(list);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 
 
@@ -20,4 +41,54 @@ public class MainActivity extends Activity {
         EditText editText = (EditText) findViewById(R.id.editText);
         textView.setText("Hello,Peter!");//+ editText.getText().toString() + "!"
     }
+
+    public class SearchItem {
+        private String keyword;
+
+        public SearchItem(String keyword) {
+            this.keyword = keyword;
+        }
+
+        public void setKeyword(String keyword) {
+            this.keyword = keyword;
+        }
+
+        public String getKeyword() {
+            return keyword;
+        }
+    }
+
+
+    class mAdapter extends BaseAdapter {
+        public void setList(List<SearchItem> list) {
+            this.list = list;
+        }
+
+        private List<SearchItem> list;
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return list.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null)
+                view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_list, null);
+            ((TextView) view.findViewById(R.id.textView_item)).setText(list.get(i).getKeyword());
+            return view;
+        }
+    }
+
+
 }
