daa3675efeeb22cfc5f31d1a645301f8782ee30b
xiaoqingbu <xiaoqingbu@sohu-inc.com>
2017-03-16 15:08:50 +0800

    change

---
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/LottieTest.java
@@ -0,0 +1,70 @@
+package com.airbnb.lottie;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.airbnb.lottie.samples.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Run these with: ./gradlew recordMode screenshotTests
+ * If you run that command, it completes successfully, and nothing shows up in git, then you
+ * haven't broken anything!
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LottieTest {
+  @Rule public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(
+      MainActivity.class);
+
+  @Test public void testAll() {
+    MainActivity activity = activityRule.getActivity();
+    TestRobot.testLinearAnimation(activity, "9squares-AlBoardman.json");
+    TestRobot.testLinearAnimation(activity, "EmptyState.json");
+    TestRobot.testLinearAnimation(activity, "HamburgerArrow.json");
+    TestRobot.testLinearAnimation(activity, "LottieLogo1.json");
+    TestRobot.testLinearAnimation(activity, "LottieLogo2.json");
+    TestRobot.testLinearAnimation(activity, "MotionCorpse-Jrcanest.json");
+    TestRobot.testLinearAnimation(activity, "PinJump.json");
+    TestRobot.testLinearAnimation(activity, "TwitterHeart.json");
+    TestRobot.testLinearAnimation(activity, "WeAccept.json", "Images/WeAccept");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Progress Success.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Touch ID.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Loading 1.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Loading 2.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Loading 3.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Loading 4.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Retweet.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Camera.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Video Camera.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Emoji Tongue.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Emoji Wink.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Emoji Shock.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Play and Like It.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Gears.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Postcard.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Preloader.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Tadah Image.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Tadah Video.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - Favorite Star.json");
+    TestRobot.testLinearAnimation(activity, "lottiefiles.com - ATM.json");
+    TestRobot.testLinearAnimation(activity, "Tests/CheckSwitch.json");
+    TestRobot.testLinearAnimation(activity, "Tests/Fill.json");
+    TestRobot.testLinearAnimation(activity, "Tests/KeyframeTypes.json");
+    TestRobot.testLinearAnimation(activity, "Tests/Laugh4.json");
+    TestRobot.testLinearAnimation(activity, "Tests/LoopPlayOnce.json");
+    TestRobot.testLinearAnimation(activity, "Tests/Parenting.json");
+    TestRobot.testLinearAnimation(activity, "Tests/Precomps.json");
+    TestRobot.testLinearAnimation(activity, "Tests/ShapeTypes.json");
+    TestRobot.testLinearAnimation(activity, "Tests/SplitDimensions.json");
+    TestRobot.testLinearAnimation(activity, "Tests/Stroke.json");
+    TestRobot.testLinearAnimation(activity, "Tests/TrackMattes.json");
+    TestRobot.testLinearAnimation(activity, "Tests/TrimPaths.json");
+    TestRobot.testChangingCompositions(activity, "TwitterHeart.json", "PinJump.json");
+    TestRobot.testSettingSameComposition(activity, "PinJump.json");
+  }
+}
---
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/TestRobot.java
@@ -0,0 +1,102 @@
+package com.airbnb.lottie;
+
+import android.support.annotation.Nullable;
+
+import com.airbnb.lottie.samples.MainActivity;
+import com.facebook.testing.screenshot.Screenshot;
+import com.facebook.testing.screenshot.ViewHelpers;
+
+import java.util.concurrent.Semaphore;
+
+class TestRobot {
+
+  static void testLinearAnimation(MainActivity activity, String fileName) {
+    testLinearAnimation(activity, fileName, null);
+  }
+
+  static void testLinearAnimation(MainActivity activity, String fileName,
+      @Nullable String imageAssetsFolder) {
+    LottieComposition composition =
+        LottieComposition.Factory.fromFileSync(activity, fileName);
+    AnimationLinearLayout view = new AnimationLinearLayout(activity);
+
+    view.setImageAssetsFolder(imageAssetsFolder);
+    view.setComposition(composition);
+
+    ViewHelpers
+        .setupView(view)
+        .layout();
+
+    String nameWithoutExtension = fileName
+        .substring(0, fileName.lastIndexOf('.'))
+        .replace(" ", "_")
+        .replace("/", "_");
+    Screenshot.snap(view)
+        .setGroup(fileName)
+        .setName(nameWithoutExtension)
+        .record();
+  }
+
+  static void testChangingCompositions(MainActivity activity, String firstFile, String secondFile) {
+    final AnimationLinearLayout view = new AnimationLinearLayout(activity);
+    loadCompositionOnView(view, null, firstFile);
+    recordScreenshots(view, "test_changing_compositions_" + firstFile);
+
+    loadCompositionOnView(view, null, secondFile);
+    recordScreenshots(view, "test_changing_compositions_" + secondFile);
+
+    cleanUpView(view, activity);
+  }
+
+  static void testSettingSameComposition(MainActivity activity, String fileName) {
+    final AnimationLinearLayout view = new AnimationLinearLayout(activity);
+    loadCompositionOnView(view, null, fileName);
+    recordScreenshots(view, "same_composition_first_run_" + fileName);
+
+    loadCompositionOnView(view, null, fileName);
+    recordScreenshots(view, "same_composition_second_run_" + fileName);
+
+    cleanUpView(view, activity);
+  }
+
+  private static void loadCompositionOnView(AnimationLinearLayout view, String imageAssetsFolder,
+      String fileName) {
+
+    LottieComposition composition =
+        LottieComposition.Factory.fromFileSync(view.getContext(), fileName);
+
+    view.setImageAssetsFolder(imageAssetsFolder);
+    view.setComposition(composition);
+
+    ViewHelpers
+        .setupView(view)
+        .layout();
+  }
+
+  private static void recordScreenshots(AnimationLinearLayout view, String fileName) {
+    String nameWithoutExtension = fileName
+        .substring(0, fileName.lastIndexOf('.'))
+        .replace(" ", "_")
+        .replace("/", "_");
+    Screenshot.snap(view)
+        .setGroup(fileName)
+        .setName(nameWithoutExtension)
+        .record();
+  }
+
+  private static void cleanUpView(final AnimationLinearLayout view, MainActivity activity) {
+    final Semaphore semaphore = new Semaphore(1);
+    activity.runOnUiThread(new Runnable() {
+      @Override public void run() {
+        view.recycleBitmaps();
+        semaphore.release();
+      }
+    });
+
+    try {
+      semaphore.acquire();
+    } catch (InterruptedException e) {
+      // Do nothing.
+    }
+  }
+}
